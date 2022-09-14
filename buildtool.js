cf86082b88376dd961a793af20d141cb50735f27
Yielding <90570076+YieldingExploiter@users.noreply.github.com>
2022-09-14 16:44:33 +0200

    fix: Trim

--- buildtool.js
+++ buildtool.js
@@ -1,2 +1,2 @@
 /* if you see this in a browser, wtf are u doing lmaooooo */
-const fs = require('fs'), path = require('path'), proc = process;fs.writeFileSync(path.resolve(proc.cwd(),'index.html'),fs.readFileSync(path.resolve(proc.cwd(),'template.html'),'utf-8').split('%INVITE%').join(fs.readFileSync(path.resolve(proc.cwd(),'INVITE'),'utf-8')))
+const fs = require('fs'), path = require('path'), proc = process;fs.writeFileSync(path.resolve(proc.cwd(),'index.html'),fs.readFileSync(path.resolve(proc.cwd(),'template.html'),'utf-8').split('%INVITE%').join(fs.readFileSync(path.resolve(proc.cwd(),'INVITE'),'utf-8').trim()))
